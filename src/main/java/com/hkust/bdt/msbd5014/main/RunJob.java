b9eb52f2939cb4fe57d53899d25c36f0e1aa0e2f
soymilk7 <patrickwanfang@gamil.com>
2024-12-16 10:33:33 +0700

    connect done

--- src/main/java/com/hkust/bdt/msbd5014/main/RunJob.java
+++ src/main/java/com/hkust/bdt/msbd5014/main/RunJob.java
@@ -8,6 +8,8 @@ import com.hkust.bdt.msbd5014.map.MyLineitemFlatMapFunction;
 import com.hkust.bdt.msbd5014.map.MyOrdersFlatMapFunction;
 import com.hkust.bdt.msbd5014.process.MyLineitemCoProcessFunction;
 import com.hkust.bdt.msbd5014.process.MyOrdersCoProcessFunction;
+import org.apache.flink.api.java.tuple.Tuple5;
+import org.apache.flink.api.java.tuple.Tuple8;
 import org.apache.flink.streaming.api.datastream.DataStreamSource;
 import org.apache.flink.streaming.api.datastream.SingleOutputStreamOperator;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
@@ -18,26 +20,30 @@ public class RunJob {
     public static void main(String[] args) throws Exception {
         StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
         env.setParallelism(1);
-        // TODO 1.1: Read DS from Customer, Orders, Lineitem tables
-        //          Customer table is the LEAF RELATION, so all tuples are alive
         DataStreamSource<String> customerFileDS = getSourceFromFile(env, "Customer");
         DataStreamSource<String> orderFileDS = getSourceFromFile(env, "Order");
         DataStreamSource<String> lineitemFileDS = getSourceFromFile(env, "Lineitem");
         SingleOutputStreamOperator<Customer> customerDS = customerFileDS.flatMap(new MyCustomerFlatMapFunction());
         SingleOutputStreamOperator<Orders> ordersDS = orderFileDS.flatMap(new MyOrdersFlatMapFunction());
-        SingleOutputStreamOperator<Lineitem> lineitemDS = lineitemFileDS.flatMap(new MyLineitemFlatMapFunction());
+        SingleOutputStreamOperator<Lineitem> lineItemDS = lineitemFileDS.flatMap(new MyLineitemFlatMapFunction());
 
 //        customerDS.print("CUSTOMER DS====================");
 //        ordersDS.print("ORDERS DS======================");
-        // TODO
-        customerDS
+//        lineItemDS.print("LineItem DS======================");
+
+        SingleOutputStreamOperator<Tuple5<String, String, String, String, String>> customerConOrder = customerDS
                 .connect(ordersDS)
                 .keyBy(c -> c.c_custkey, o -> o.o_custkey)
-                .process(new MyOrdersCoProcessFunction())
-//                .connect(lineitemDS)
-//                .keyBy(t -> t.f1, l -> l.l_orderkey)
-//                .process(new MyLineitemCoProcessFunction())
-                .print();
+                .process(new MyOrdersCoProcessFunction());
+
+        customerConOrder.print("customerConOrder: ");
+
+        SingleOutputStreamOperator<Tuple8<String, String, String, String, String, Double, Double, String>> customerConOrderConLineItem = customerConOrder
+                .connect(lineItemDS)
+                .keyBy(t -> t.f1, l -> l.l_orderkey)
+                .process(new MyLineitemCoProcessFunction());
+
+        customerConOrderConLineItem.print("customerConOrderConLineItem: ");
         env.execute();
     }
 }
--- src/main/java/com/hkust/bdt/msbd5014/process/MyLineitemCoProcessFunction.java
+++ src/main/java/com/hkust/bdt/msbd5014/process/MyLineitemCoProcessFunction.java
@@ -1,25 +1,65 @@
 package com.hkust.bdt.msbd5014.process;
 
 import com.hkust.bdt.msbd5014.bean.Lineitem;
+import org.apache.flink.api.common.state.ListState;
+import org.apache.flink.api.common.state.ListStateDescriptor;
+import org.apache.flink.api.common.typeinfo.Types;
 import org.apache.flink.api.java.tuple.Tuple5;
 import org.apache.flink.api.java.tuple.Tuple8;
+import org.apache.flink.configuration.Configuration;
 import org.apache.flink.streaming.api.functions.co.CoProcessFunction;
 import org.apache.flink.util.Collector;
 
 /**
- * In1: Tuple5
- * In2: Lineitem
- * Out: Tuple8(custkey, orderkey, mktsegment, orderdate, sp, ep, disct, shipdate)
+ * 多对多连接逻辑
+ * 输入流1: Tuple5(custkey, orderkey, mktsegment, orderdate, sp)
+ * 输入流2: Lineitem(l_orderkey, l_ep, l_dis, l_shipdate)
+ * 输出流: Tuple8(custkey, orderkey, mktsegment, orderdate, sp, ep, disct, shipdate)
  */
 public class MyLineitemCoProcessFunction extends CoProcessFunction<Tuple5<String, String, String, String, String>, Lineitem, Tuple8<String, String, String, String, String, Double, Double, String>> {
 
-    @Override
-    public void processElement1(Tuple5<String, String, String, String, String> value, CoProcessFunction<Tuple5<String, String, String, String, String>, Lineitem, Tuple8<String, String, String, String, String, Double, Double, String>>.Context ctx, Collector<Tuple8<String, String, String, String, String, Double, Double, String>> out) throws Exception {
+    private ListState<Tuple5> ordersState;
+    private ListState<Lineitem> lineitemState;
 
+    @Override
+    public void open(Configuration parameters) throws Exception {
+        ordersState = getRuntimeContext().getListState(
+                new ListStateDescriptor<>("ordersState", Types.GENERIC(Tuple5.class)));
+        lineitemState = getRuntimeContext().getListState(
+                new ListStateDescriptor<>("lineitemState", Types.GENERIC(Lineitem.class)));
     }
 
     @Override
-    public void processElement2(Lineitem value, CoProcessFunction<Tuple5<String, String, String, String, String>, Lineitem, Tuple8<String, String, String, String, String, Double, Double, String>>.Context ctx, Collector<Tuple8<String, String, String, String, String, Double, Double, String>> out) throws Exception {
+    public void processElement1(Tuple5<String, String, String, String, String> order,
+                                Context ctx,
+                                Collector<Tuple8<String, String, String, String, String, Double, Double, String>> out) throws Exception {
+        // 将当前 order 加入状态
+        ordersState.add(order);
 
+        // 检查状态中是否存在匹配的 Lineitem 数据
+        for (Lineitem lineitem : lineitemState.get()) {
+            if (order.f1.equals(lineitem.l_orderkey)) {
+                out.collect(new Tuple8<>(
+                        order.f0, order.f1, order.f2, order.f3, order.f4,
+                        lineitem.l_ep, lineitem.l_dis, lineitem.l_shipdate));
+            }
+        }
+    }
+
+    @Override
+    public void processElement2(Lineitem lineitem,
+                                Context ctx,
+                                Collector<Tuple8<String, String, String, String, String, Double, Double, String>> out) throws Exception {
+        // 将当前 Lineitem 加入状态
+        lineitemState.add(lineitem);
+
+        // 检查状态中是否存在匹配的 Order 数据
+        for (Tuple5<String, String, String, String, String> order : ordersState.get()) {
+            if (lineitem.l_orderkey.equals(order.f1)) {
+                out.collect(new Tuple8<>(
+                        order.f0, order.f1, order.f2, order.f3, order.f4,
+                        lineitem.l_ep, lineitem.l_dis, lineitem.l_shipdate));
+            }
+        }
     }
 }
